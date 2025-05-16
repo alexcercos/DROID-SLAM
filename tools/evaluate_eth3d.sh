4312e8802bf5afb7c9052bcc91c81a89994053a2
alexcercos <alexcercos@gmail.com>
2025-05-16 11:37:27 +0200

    Comment error datasets
    Run on server

--- tools/evaluate_eth3d.sh
+++ tools/evaluate_eth3d.sh
@@ -8,33 +8,33 @@ evalset=(
     cables_1
     cables_2
     cables_3
-    camera_shake_1
-    camera_shake_2
-    camera_shake_3
+#    camera_shake_1
+#    camera_shake_2
+#    camera_shake_3
     ceiling_1
-    ceiling_2
-    desk_3
-    desk_changing_1
+#    ceiling_2
+#    desk_3
+#    desk_changing_1
     einstein_1
     einstein_2
     # einstein_dark
-    einstein_flashlight
+#    einstein_flashlight
     einstein_global_light_changes_1
     einstein_global_light_changes_2
-    einstein_global_light_changes_3
+#    einstein_global_light_changes_3
     kidnap_1
     # kidnap_dark
     large_loop_1
     mannequin_1
     mannequin_3
     mannequin_4
-    mannequin_5
+#    mannequin_5
     mannequin_7
     mannequin_face_1
     mannequin_face_2
     mannequin_face_3
-    mannequin_head
-    motion_1
+#    mannequin_head
+#    motion_1
     planar_2
     planar_3
     plant_1
@@ -46,8 +46,8 @@ evalset=(
     plant_scene_1
     plant_scene_2
     plant_scene_3
-    reflective_1
-    repetitive
+#    reflective_1
+#    repetitive
     sfm_bench
     sfm_garden
     sfm_house_loop
